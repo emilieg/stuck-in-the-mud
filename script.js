3e1fc65368569903ebaad1f55a4009fb524467c1
emilieg <emilie.gerber@hotmail.com>
2016-04-04 13:05:15 -0700

    added logic to calculate points

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 //Global variables
 //DiceBoard
 
-var currentScore;
+var playerTurn = player1;
 var diceCount = ['','','','','']; //5 dices are available at the start
-var currentDice = []; //push dice values into currentDice array
+var currentRollPoints =[];
+var cleanRoll;
+var playerScore;
 
+
+
+var flipPlayerTurn = function(){
+  if (playerTurn === 'player1') {
+      playerTurn = 'player2';
+  }  else {
+    playerTurn = 'player1';
+  } 
+}
 
 //Roll Dice function
 var rollDice = function(){
@@ -17,29 +28,65 @@ $( document ).ready(function() {
 
 
   $('#roll-dice').click(function() {
-//Call Roll Dice function for each die using a for loop
+    //Call Roll Dice function for each die using a for loop
+    cleanRoll = true; 
+    for (i=0; i< diceCount.length; i++) {
 
-for (i=0; i< diceCount.length; i++) {
-  console.log("diceCount.lenght is: " + diceCount.length);
-  
-  if(diceCount[i] !==2 && diceCount[i] !== 5) {
 
-    diceCount[i] = rollDice();
-    console.log(diceCount[i]);
-    var diceId = $('#die' + (i+1)).html(diceCount[i]);
-    
-    if(diceCount[i] === 2 || diceCount[i] === 5) {
-      $(diceId).css('border-color', 'red');
-
+      if(diceCount[i] !==2 && diceCount[i] !== 5) {
+        diceCount[i] = rollDice();
+        console.log(diceCount[i]);
+        var diceId = $('#die' + (i+1)).html(diceCount[i]);
+       
+        if(diceCount[i] === 2 || diceCount[i] === 5) {
+          $(diceId).css('border-color', 'red');  
+          cleanRoll = false;
+        }
+      } 
     }
-  }
+    if(cleanRoll === true) {
+      var turnSum = diceSum(diceCount);
+      currentRollPoints.push(turnSum);
+    
+      console.log( "The turnSum is: " + turnSum);
+      console.log( "The currentRollPoints is: " + currentRollPoints);
+    }
+    //check if all dice are stuck/equal 2 or 5
+    //then calculate player1Score
+    if(cleanRoll === false) {
+    playerScore = pointsSum(currentRollPoints);
+    console.log("playerScore :" + playerScore);
+    
+        }
+      
 
-}
-
-}) //closing click function
+  }) //closing click function
 }); //closing for document_ready f()
 
 
+
+function diceSum(diceCount){
+  var total = 0;
+  len = diceCount.length;
+
+  for (var i = 0; i < len; i++){
+    if(diceCount[i] !==2 && diceCount[i] !==5){
+      total += diceCount[i];
+    }
+  }
+  return total;
+};
+
+
+
+function pointsSum(currentRollPoints){
+  var total =0;
+  len = currentRollPoints.length;
+
+  for(var i = 0; i <len; i++) {
+    total+= currentRollPoints[i];
+  } return total;
+}
 
 
 
